61047a3d30aba292897aba4074333e495681b903
MT1EL <tokochixladze54@gmail.com>
2022-09-01 14:25:33 -0700

    fixed token validation

--- src/store/reducers/postUserReducer.js
+++ src/store/reducers/postUserReducer.js
@@ -22,8 +22,8 @@ export default function postUserReducer(state = initialState, action) {
           position_id: +action.payload.user?.position_id,
           email: action.payload.user?.email,
           phone_number: action.payload.user?.number,
-          token: action.payload.user.number
-            ? action.payload.user?.number
+          token: action.payload.user.token
+            ? action.payload.user?.token
             : "7de1619aa2a1cf2a4369c6512551c306",
         },
       };
